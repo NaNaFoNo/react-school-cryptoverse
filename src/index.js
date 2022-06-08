08661e17610ac07956b28760ff7839decd271d61
Andreas Vieten <cryptonari@outlook.com>
2022-06-08 21:57:09 +0200

    add: First Redux API Call

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import App from './App';
-import 'antd/dist/antd.min.css'
+import store from './app/store';
+import 'antd/dist/antd.min.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
     <Router>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </Router>
 );
---
+++ src/services/cryptoApi.js
@@ -0,0 +1,26 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const API_KEY = process.env.REACT_APP_RAPID_API_KEY;
+
+const cryptoApiHeaders = {
+    'X-RapidAPI-Key': API_KEY,
+    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: () => createRequest('/coins')
+        })
+    })
+});
+
+export const {
+    useGetCryptosQuery,
+} = cryptoApi;
