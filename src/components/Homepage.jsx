1ad15529281fd84060401436c05b1dd6062c18a5
Andreas Vieten <cryptonari@outlook.com>
2022-06-09 11:18:28 +0200

    fix: Homepage headers classnames

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -25,12 +25,12 @@ const Homepage = () => {
                 <Col span={12}><Statistic title="Total 24h Volume" value={ millify(globalStats.total24hVolume) }/></Col>
                 <Col span={12}><Statistic title="Total Markets" value={ millify(globalStats.totalMarkets) }/></Col>
             </Row>
-            <div>
+            <div className="home-heading-container">
                 <Title level={2} className="home-title">Top 10 Cryptocurrencies in the world</Title>
                 <Title level={3} className="show-more"><Link to="/cryptocurrencies">Show more</Link></Title>
             </div>
             <Cryptocurrencies simplified/>
-            <div>
+            <div className="home-heading-container">
                 <Title level={2} className="home-title">Latest Crypto News</Title>
                 <Title level={3} className="show-more"><Link to="/news">Show more</Link></Title>
             </div>
