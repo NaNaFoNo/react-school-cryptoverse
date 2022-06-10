6cc54c0fd3ea9b3c080bc56a3e9ce6fb2bd312c4
Andreas Vieten <cryptonari@outlook.com>
2022-06-10 10:58:59 +0200

    add: News Search Filter by Coin

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Select, Typography, Row, Col, Avatar, Card } from 'antd';
 import moment from 'moment';
 
 import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi';
+import { useGetCryptosQuery } from '../services/cryptoApi';
 
 const { Text, Title } = Typography;
 const { Option } = Select;
@@ -10,13 +11,32 @@ const { Option } = Select;
 const demoImage = 'https://picsum.photos/420/320?image=0';
  
 const News = ({ simplified }) => {
+    const [newsCategory, setNewsCategory] = useState('Cryptocurrency')
     const count = simplified ? 6 : 12;
-    const { data: cryptoNews } = useGetCryptoNewsQuery({newsCatagory: "", count: count});
+    const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory, count: count});
+    const { data } = useGetCryptosQuery(100);
 
     if(!cryptoNews?.value) return 'Loading....';
 
     return (
         <Row gutter={[24, 24]}>
+            {!simplified && (
+                <Col span={24}>
+                    <Select
+                        showSearch
+                        className="select-news"
+                        placeholder="Select a Crypto"
+                        optionFilterProp="children"
+                        onChange={(value) => setNewsCategory(value)}
+                        filterOption={(input, option) => option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
+                    >
+                        <Option value="Cryptocurrency">Cryptocurrency</Option>
+                        {data?.data?.coins.map((coin) => (
+                            <Option value={coin.name}>{coin.name}</Option>
+                        ))}
+                    </Select>
+                </Col>
+            )}
             {cryptoNews?.value.map((news, i) => (
                 <Col xs={24} sm={12} lg={6} key={i}>
                     <Card hoverable className="news-card">
